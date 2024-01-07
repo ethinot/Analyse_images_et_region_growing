f984e06dbb904ff56af23c319c1482e288009376
To <theogrillon6f9@gmail.com>
2024-01-07 17:45:55 +0100

    modif merge

--- tp1/tp1.cpp
+++ tp1/tp1.cpp
@@ -149,35 +149,51 @@ cv::Scalar max(cv::Scalar const& sa, cv::Scalar const& sb)
 void update_buffer(cv::Mat & buffer, std::list<cv::Point> const& points, int newValue)
 {
     for (auto const& point : points) {
+
         buffer.at<int>(point) = newValue;
     }
 }
 
-// O(1)
-void merge(region_container & regions, int hexR1, int hexR2)
+int cpt = 0;
+
+// O(min(size1, size2))
+void merge(region_container & regions, cv::Mat & buffer, int & r1Key, int & r2Key)
 {
-    std::vector<cv::Scalar> hsv1 = regions[hexR1].second;
-    std::vector<cv::Scalar> hsv2 = regions[hexR2].second;
-    hsv1[0] = min(hsv1[0], hsv2[0]);
-    hsv1[1] = max(hsv1[1], hsv2[1]);
+    std::vector<cv::Scalar> hsv1 = regions[r1Key].second;
+    std::vector<cv::Scalar> hsv2 = regions[r2Key].second;
+    std::vector<cv::Scalar> hsv(3, 0);
+    hsv[0] = min(hsv1[0], hsv2[0]);
+    hsv[1] = max(hsv1[1], hsv2[1]);
 
-    int size1 = (int)regions[hexR1].first.size();
-    int size2 = (int)regions[hexR2].first.size();
-    hsv1[2] = (hsv1[2]*size1 + hsv2[2]*size2) / (size1 + size2);
-    regions[hexR1].second = hsv1;
+    int size1 = (int)regions[r1Key].first.size();
+    int size2 = (int)regions[r2Key].first.size();
+    hsv[2] = (hsv1[2]*size1 + hsv2[2]*size2) / (size1 + size2);
 
-    regions[hexR1].first.splice(
-            regions[hexR1].first.end(), regions[hexR2].first);
+    if (size1 < size2) {
+        update_buffer(buffer, regions[r1Key].first, r2Key);
+        regions[r2Key].first.splice(
+                regions[r2Key].first.end(), regions[r1Key].first);
+        regions.erase(regions.find(r1Key));
+        regions[r2Key].second = hsv;
+        r1Key = r2Key;
+    } else {
+        update_buffer(buffer, regions[r2Key].first, r1Key);
+        regions[r1Key].first.splice(
+                regions[r1Key].first.end(), regions[r2Key].first);
+        regions.erase(regions.find(r2Key));
+        regions[r1Key].second = hsv;
+        r2Key = r1Key;
+    }
 
-    regions.erase(regions.find(hexR2));
+    ++cpt;
 }
 
 // O(1)
 void process(region_container & regions, std::vector<cv::Mat> const& hsvChannels,
-             cv::Mat & buffer, std::queue<cv::Point> & queue, cv::Point const& current, int hexKey)
+             cv::Mat & buffer, std::queue<cv::Point> & queue, cv::Point const& current, int & currentKey)
 {
-    cv::Scalar lowerb = regions[hexKey].second[0];
-    cv::Scalar upperb = regions[hexKey].second[1];
+    cv::Scalar lowerb = regions[currentKey].second[0];
+    cv::Scalar upperb = regions[currentKey].second[1];
 
     for (int i = -1; i <= 1; ++i) {
         for (int j = -1; j <= 1; ++j) {
@@ -185,28 +201,29 @@ void process(region_container & regions, std::vector<cv::Mat> const& hsvChannels
                 cv::Point neighbor(current.x + i, current.y + j);
                 if (neighbor.x >= 0 && neighbor.x < hsvChannels[0].cols &&
                     neighbor.y >= 0 && neighbor.y < hsvChannels[0].rows) {
-                    int hexNeighbor = buffer.at<int>(neighbor);
-                    if (hexNeighbor == 0) {
+                    int neighborKey = buffer.at<int>(neighbor);
+                    if (neighborKey == 0) {
                         auto neighborH = (double) hsvChannels[0].at<uchar>(neighbor);
                         auto neighborS = (double) hsvChannels[1].at<uchar>(neighbor);
                         auto neighborV = (double) hsvChannels[2].at<uchar>(neighbor);
                         cv::Scalar hsvNeighbor(neighborH, neighborS, neighborV);
 
                         if (predicate(lowerb, upperb, hsvNeighbor)) {
-                            buffer.at<int>(neighbor) = hexKey;
-                            update_mean(regions[hexKey], hsvNeighbor);
-                            regions[hexKey].first.push_back(neighbor);
+                            buffer.at<int>(neighbor) = currentKey;
+                            update_mean(regions[currentKey], hsvNeighbor);
+                            regions[currentKey].first.push_back(neighbor);
                             queue.push(neighbor);
                         }
-                    } else if (hexKey != hexNeighbor) {
+                    } else if (currentKey != neighborKey) {
                         std::vector<cv::Scalar> hsvNeighborRegion =
-                                regions[hexNeighbor].second;
+                                regions[neighborKey].second;
                         cv::Scalar currentRegionHsvMean =
-                                regions[hexKey].second[2];
+                                regions[currentKey].second[2];
                         if (predicate(lowerb, upperb, hsvNeighborRegion[2]) &&
                             predicate(hsvNeighborRegion[0], hsvNeighborRegion[1], currentRegionHsvMean)) {
-                            update_buffer(buffer, regions[hexNeighbor].first, hexKey);
-                            merge(regions, hexKey, hexNeighbor);
+                            merge(regions, buffer, currentKey, neighborKey);
+
+
                         }
                     }
                 }
@@ -217,7 +234,7 @@ void process(region_container & regions, std::vector<cv::Mat> const& hsvChannels
 
 // O(w) avec w nombre de pixels parcourus
 void growing(region_container & regions, std::vector<cv::Mat> const& hsvChannels,
-             cv::Mat & buffer, cv::Point const& seed, int hexKey)
+             cv::Mat & buffer, cv::Point const& seed, int currentKey)
 {
     std::queue<cv::Point> queue;
     queue.push(seed);
@@ -228,18 +245,18 @@ void growing(region_container & regions, std::vector<cv::Mat> const& hsvChannels
 
     cv::Scalar hsvSeed(seedH, seedS, seedV);
     std::pair<cv::Scalar, cv::Scalar> bounds = interval_bounds(hsvSeed);
-    regions[hexKey].second.emplace_back(bounds.first);
-    regions[hexKey].second.emplace_back(bounds.second);
-    regions[hexKey].second.emplace_back(0);
-    update_mean(regions[hexKey], hsvSeed);
+    regions[currentKey].second.emplace_back(bounds.first);
+    regions[currentKey].second.emplace_back(bounds.second);
+    regions[currentKey].second.emplace_back(0);
+    update_mean(regions[currentKey], hsvSeed);
 
-    regions[hexKey].first.push_back(seed);
-    buffer.at<int>(seed) = hexKey;
+    regions[currentKey].first.push_back(seed);
+    buffer.at<int>(seed) = currentKey;
 
     while (!queue.empty()) {
         cv::Point current = queue.front();
         queue.pop();
-        process(regions, hsvChannels, buffer, queue, current, hexKey);
+        process(regions, hsvChannels, buffer, queue, current, currentKey);
     }
 }
 
@@ -331,9 +348,13 @@ void seg(cv::Mat const& src, cv::Mat & dst, std::vector<cv::Point> const& seeds)
     std::vector<cv::Vec3b> colorList = generate_random_unique_BGR(numSeeds);
 
     for (size_t i = 0; i < numSeeds; ++i) {
-        growing(regions, hsvChannels, buffer, seeds[i], bgr_to_hex(colorList[i]));
+        int currentKey = bgr_to_hex(colorList[i]);
+        growing(regions, hsvChannels, buffer, seeds[i], currentKey);
 //        growing(regions, hsvChannels, buffer, seeds[i], colorList[i]);
+        cpt;
     }
+
+    fill_mask(dst, regions);
 }
 
 int main(int argc, char** argv) {
@@ -398,13 +419,13 @@ int main(int argc, char** argv) {
 
     cv::waitKey(0);*/
 
-    /*cv::Mat hsvImg;
-    cv::cvtColor(image, hsvImg, cv::COLOR_BGR2HSV);
+//    cv::Mat hsvImg;
+//    cv::cvtColor(image, hsvImg, cv::COLOR_BGR2HSV);
+//
+//    std::vector<cv::Mat> hsvChannels;
+//    cv::split(hsvImg, hsvChannels);
 
-    std::vector<cv::Mat> hsvChannels;
-    cv::split(hsvImg, hsvChannels);
-
-    cv::Point coords(45,303);
+    /*cv::Point coords(45,303);
     uchar H = hsvChannels[0].at<uchar>(coords);
     uchar S = hsvChannels[1].at<uchar>(coords);
     uchar V = hsvChannels[2].at<uchar>(coords);
@@ -420,75 +441,91 @@ int main(int argc, char** argv) {
 
     cv::imshow("Seg with HSV space color test", result);
 
-    cv::waitKey(0);*/
+    cv::waitKey(0);
 
-//    cv::Point test(206,274);
-//    uchar valueH = hsvChannels[0].at<uchar>(test);
-//    uchar valueS = hsvChannels[1].at<uchar>(test);
-//    uchar valueV = hsvChannels[2].at<uchar>(test);
+    cv::Point test(206,274);
+    uchar valueH = hsvChannels[0].at<uchar>(test);
+    uchar valueS = hsvChannels[1].at<uchar>(test);
+    uchar valueV = hsvChannels[2].at<uchar>(test);
+
+    cv::Scalar value(valueH, valueS, valueV);
+
+    bool res = predicate(lowerb, upperb, value);*/
+
+//    region_container regions;
 //
-//    cv::Scalar value(valueH, valueS, valueV);
-
-//    bool res = predicate(lowerb, upperb, value);
-
-    /*region_container regions;
-
-    int hexR1 = bgr_to_hex(cv::Vec3b(255, 0, 0));
-    int hexR2 = bgr_to_hex(cv::Vec3b(0, 0, 255));
-
-    cv::Point pos1(173,255);
-    auto H = (double)hsvChannels[0].at<uchar>(pos1);
-    auto S = (double)hsvChannels[1].at<uchar>(pos1);
-    auto V = (double)hsvChannels[2].at<uchar>(pos1);
-    cv::Scalar hsv1(H,S,V);
-    regions[hexR1].second = {cv::Scalar(0), cv::Scalar(0), cv::Scalar(0)};
-    std::pair<cv::Scalar, cv::Scalar> bounds = interval_bounds(hsv1);
-    regions[hexR1].second[0] = bounds.first;
-    regions[hexR1].second[1] = bounds.second;
-    update_mean(regions[hexR1], hsv1);
-    regions[hexR1].first.push_back(pos1);
-
-    cv::Point pos2(202,279);
-    H = (double)hsvChannels[0].at<uchar>(pos2);
-    S = (double)hsvChannels[1].at<uchar>(pos2);
-    V = (double)hsvChannels[2].at<uchar>(pos2);
-    cv::Scalar hsv2(H,S,V);
-    regions[hexR2].second = {cv::Scalar(0), cv::Scalar(0), cv::Scalar(0)};
-    bounds = interval_bounds(hsv2);
-    regions[hexR2].second[0] = bounds.first;
-    regions[hexR2].second[1] = bounds.second;
-    update_mean(regions[hexR2], hsv2);
-    regions[hexR2].first.push_back(pos2);
-
-    for (int i = 0; i < 100; ++i) {
-        cv::Point p1(8, 7);
-        H = (double)hsvChannels[0].at<uchar>(p1);
-        S = (double)hsvChannels[1].at<uchar>(p1);
-        V = (double)hsvChannels[2].at<uchar>(p1);
-        cv::Scalar tmp_hsv1(H,S,V);
-
-        cv::Point p2(8, 7);
-        H = (double)hsvChannels[0].at<uchar>(p2);
-        S = (double)hsvChannels[1].at<uchar>(p2);
-        V = (double)hsvChannels[2].at<uchar>(p2);
-        cv::Scalar tmp_hsv2(H,S,V);
-
-        update_mean(regions[hexR1], tmp_hsv1);
-        regions[hexR1].first.push_back(p1);
-        update_mean(regions[hexR2], tmp_hsv2);
-        regions[hexR2].first.push_back(p2);
-    }
-
-    merge(regions, hexR1, hexR2);*/
+//    int hexR1 = bgr_to_hex(cv::Vec3b(255, 0, 0));
+//    int hexR2 = bgr_to_hex(cv::Vec3b(0, 0, 255));
+//
+//    cv::Point pos1(173,255);
+//    auto H = (double)hsvChannels[0].at<uchar>(pos1);
+//    auto S = (double)hsvChannels[1].at<uchar>(pos1);
+//    auto V = (double)hsvChannels[2].at<uchar>(pos1);
+//    cv::Scalar hsv1(H,S,V);
+//    regions[hexR1].second = {cv::Scalar(0), cv::Scalar(0), cv::Scalar(0)};
+//    std::pair<cv::Scalar, cv::Scalar> bounds = interval_bounds(hsv1);
+//    regions[hexR1].second[0] = bounds.first;
+//    regions[hexR1].second[1] = bounds.second;
+//    update_mean(regions[hexR1], hsv1);
+//    regions[hexR1].first.push_back(pos1);
+//
+//    cv::Point pos2(202,279);
+//    H = (double)hsvChannels[0].at<uchar>(pos2);
+//    S = (double)hsvChannels[1].at<uchar>(pos2);
+//    V = (double)hsvChannels[2].at<uchar>(pos2);
+//    cv::Scalar hsv2(H,S,V);
+//    regions[hexR2].second = {cv::Scalar(0), cv::Scalar(0), cv::Scalar(0)};
+//    bounds = interval_bounds(hsv2);
+//    regions[hexR2].second[0] = bounds.first;
+//    regions[hexR2].second[1] = bounds.second;
+//    update_mean(regions[hexR2], hsv2);
+//    regions[hexR2].first.push_back(pos2);
+//
+//    for (int i = 0; i < 50; ++i) {
+//        cv::Point p1(8, 7);
+//        H = (double)hsvChannels[0].at<uchar>(p1);
+//        S = (double)hsvChannels[1].at<uchar>(p1);
+//        V = (double)hsvChannels[2].at<uchar>(p1);
+//        cv::Scalar tmp_hsv1(H,S,V);
+//
+//        update_mean(regions[hexR1], tmp_hsv1);
+//        regions[hexR1].first.push_back(p1);
+//    }
+//
+//    for (int i = 0; i < 100; ++i) {
+//        cv::Point p2(8, 7);
+//        H = (double)hsvChannels[0].at<uchar>(p2);
+//        S = (double)hsvChannels[1].at<uchar>(p2);
+//        V = (double)hsvChannels[2].at<uchar>(p2);
+//        cv::Scalar tmp_hsv2(H,S,V);
+//
+//        update_mean(regions[hexR2], tmp_hsv2);
+//        regions[hexR2].first.push_back(p2);
+//    }
+//
+//    cv::Mat buffer = cv::Mat::zeros(image.size(), CV_32S);
+//    merge(regions, buffer, hexR1, hexR2);
+//
+//    for (int i = 0; i < 50; ++i) {
+//        cv::Point p1(8, 7);
+//        H = (double)hsvChannels[0].at<uchar>(p1);
+//        S = (double)hsvChannels[1].at<uchar>(p1);
+//        V = (double)hsvChannels[2].at<uchar>(p1);
+//        cv::Scalar tmp_hsv1(H,S,V);
+//
+//        update_mean(regions[hexR1], tmp_hsv1);
+//        regions[hexR1].first.push_back(p1);
+//    }
 
     cv::Mat mask = cv::Mat::zeros(image.size(), CV_8UC3);
+
     auto start = std::chrono::high_resolution_clock::now();
     seg(image, mask, seeds);
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     std::cout << (duration.count() / 1000.0) << "ms" << std::endl;
 
-    cv::namedWindow("Segmentation", cv::WINDOW_NORMAL);
+//    cv::namedWindow("Segmentation", cv::WINDOW_NORMAL);
     cv::imshow("Segmentation", mask);
     cv::waitKey(0);
     return 0;
