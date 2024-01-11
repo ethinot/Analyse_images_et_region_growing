b17cd103e09165b5eaabfda77681b53edfec3930
p1909945 <edouard.thinot@etu.univ-lyon1.fr>
2024-01-11 12:06:37 +0100

    Create a new file to implements operation on the segmentedRegion objet (2 points and the associate variance).

---
+++ tp1/SegmentedRegion.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <list>
+#include <iostream>
+
+class SegmentedRegion {
+private:
+    cv::Point topLeft;
+    cv::Point bottomRight;
+    double variance;
+
+public:
+    SegmentedRegion();
+
+    SegmentedRegion(cv::Point &, cv::Point &, double &);
+    
+    ~SegmentedRegion();
+
+    const double getVariance() const;
+    void setVariance(double &);
+
+    const cv::Point getTopLeftPoint() const;
+    void setTopLeftPoint(cv::Point &);
+
+    const cv::Point getBottomRightPoint() const;
+    void setBottomRightPoint(cv::Point &);
+
+};
+
+SegmentedRegion::SegmentedRegion() : topLeft(0, 0), bottomRight(0, 0), variance(0.0) { }
+
+SegmentedRegion::SegmentedRegion(cv::Point & _topLeft, cv::Point & _bottomRight, double & _variance) : topLeft(_topLeft), bottomRight(_bottomRight), variance(_variance) { }
+
+SegmentedRegion::~SegmentedRegion() { }
+
+const double SegmentedRegion::getVariance() const {
+    return this->variance;
+}
+
+void SegmentedRegion::setVariance(double & newVariance) {
+    this->variance = newVariance;
+}
+
+const cv::Point SegmentedRegion::getTopLeftPoint() const {
+    return this->topLeft;
+}
+void SegmentedRegion::setTopLeftPoint(cv::Point & newTopLeft) {
+    this->topLeft = newTopLeft;
+}
+
+const cv::Point SegmentedRegion::getBottomRightPoint() const {
+    return this->bottomRight;
+
+}
+
+void SegmentedRegion::setBottomRightPoint(cv::Point & newBottomRight) {
+    this->bottomRight = newBottomRight;
+}
