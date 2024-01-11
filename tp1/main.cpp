40a5dac4293270914ea2be2558fc76b18cad6589
p1909945 <edouard.thinot@etu.univ-lyon1.fr>
2024-01-11 17:58:40 +0100

    Test segmentation (divide_image).

--- tp1/main.cpp
+++ tp1/main.cpp
@@ -12,22 +12,22 @@ int main(int argc, char** argv) {
     ImageProcessor imageProcessor;
     imageProcessor.process_image(argv[1]);
 
-    cv::Point point1(10, 20);
-    cv::Point point2(30, 40);
-    double variance = 3.8;
-
-    SegmentedRegion pv1(point1, point2, variance);
-
-    // Accès aux données
-    std::cout << point1 << ", " << point2 << ", on une variance de : " << variance <<std::endl;
-
     ImageUtil imageUtil;
-    double varianceGrayscale, varianceHsv;
-    varianceGrayscale = imageUtil.calculate_variance(imageProcessor.get_image_rgb(), 0);
-    varianceHsv = imageUtil.calculate_variance(imageProcessor.get_image_rgb(), 1);
+    double variance;
 
-    std::cout<<"Variance GrayScale : " << varianceGrayscale << std::endl;
-    std::cout<<"Variance HSV : " << varianceHsv << std::endl;
+    GermsPositioningV2 germsPositioning;
+
+    cv::Point initialTopLeft(0, 0);
+    cv::Point initialBottomRight(imageProcessor.get_image_rgb().cols, imageProcessor.get_image_rgb().rows);
+
+    variance = imageUtil.calculate_region_variance(imageProcessor.get_image_rgb(), initialTopLeft, initialBottomRight);
+
+    std::cout << " Variance la la region primaire : " << variance <<std::endl;
+
+    germsPositioning.divide_image(imageProcessor.get_image_rgb(), initialTopLeft, initialBottomRight, 7);
+
+    std::cout << "Nombre de sous division : " << germsPositioning.get_germs_regions().size() << std::endl;
+
 
     return 0;
 }
