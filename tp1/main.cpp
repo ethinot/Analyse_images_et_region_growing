5f71bfe4c539c20283e4289f471da1fa5ebc1cb1
p1909945 <edouard.thinot@etu.univ-lyon1.fr>
2024-01-12 10:46:42 +0100

    Test the region growing algorithm with de V1 seeds pose.

--- tp1/main.cpp
+++ tp1/main.cpp
@@ -1,37 +1,73 @@
 #include "ImageProcessor.hpp"
 #include "SegmentedRegion.hpp"
 #include "GermsPositioning.hpp"
+#include "GrowAndMerge.hpp"
 #include "ImageUtil.hpp"
 
 int main(int argc, char** argv) {
+//    if (argc < 2) {
+//        printf("Enter relative path to an image.\n");
+//        return -1;
+//    }
+//
+//    ImageProcessor imageProcessor;
+//    imageProcessor.process_image(argv[1]);
+//
+//    ImageUtil imageUtil;
+//    double varianceH, varianceS, varianceV;
+//
+//    GermsPositioningV2 germsPositioning;
+//
+//    varianceH = imageUtil.calculate_channel_variance(imageProcessor.get_image_hsv(), 0);
+//    varianceS = imageUtil.calculate_channel_variance(imageProcessor.get_image_hsv(), 1);
+//    varianceV = imageUtil.calculate_channel_variance(imageProcessor.get_image_hsv(), 2);
+//
+//    std::cout << " Variance H (teinte) : " << varianceH <<std::endl;
+//    std::cout << " Variance S (Sat) : " << varianceS <<std::endl;
+//    std::cout << " Variance V (Value) : " << varianceV <<std::endl;
+//
+//    cv::Mat image = imageProcessor.get_image_rgb();
+//    std::vector<cv::Point> seeds = germsPositioning.position_germs(image, 4);
+//
+//    std::cout << "Nombre de sous division : " << germsPositioning.get_germs_regions().size() << std::endl;
+//    std::cout << "Nombre de germe : " << seeds.size() << std::endl;
+
+    //    auto start = std::chrono::high_resolution_clock::now();
+
+    // Test growing
+
+    GrowAndMerge growAndMerge;
+
     if (argc < 2) {
-        printf("Enter relative path to an image.\n");
+        printf("usage: DisplayImage.out <Image_Path> (<num of seeds>)\n");
         return -1;
     }
 
-    ImageProcessor imageProcessor;
-    imageProcessor.process_image(argv[1]);
+    cv::Mat image;
+    image = cv::imread(argv[1], cv::IMREAD_COLOR);
 
-    ImageUtil imageUtil;
-    double variance, varianceH, varianceS, varianceV;
+    if (!image.data) {
+        printf("No image data \n");
+        return -1;
+    }
 
-    GermsPositioningV2 germsPositioning;
+    if (argc == 3) {
+        growAndMerge.set_num_seeds(strtol(argv[2], nullptr, 10));
+    }
 
-    varianceH = imageUtil.calculate_channel_variance(imageProcessor.get_image_hsv(), 0);
-    varianceS = imageUtil.calculate_channel_variance(imageProcessor.get_image_hsv(), 1);
-    varianceV = imageUtil.calculate_channel_variance(imageProcessor.get_image_hsv(), 2);
+    GermsPositioningV1 positioningV1;
 
-    std::cout << " Variance de la region primaire : " << variance <<std::endl;
-    std::cout << " Variance H (teinte) : " << varianceH <<std::endl;
-    std::cout << " Variance S (Sat) : " << varianceS <<std::endl;
-    std::cout << " Variance V (Value) : " << varianceV <<std::endl;
+    std::vector<cv::Point> seeds;
 
-    cv::Mat image = imageProcessor.get_image_rgb();
-    std::vector<cv::Point> seeds = germsPositioning.position_germs(image, 5);
+    positioningV1.generate_seed(seeds, image.cols, image.rows, growAndMerge.get_num_seeds());
 
-    std::cout << "Nombre de sous division : " << germsPositioning.get_germs_regions().size() << std::endl;
-    std::cout << "Nombre de germe : " << seeds.size() << std::endl;
+    cv::Mat mask = cv::Mat::zeros(image.size(), CV_8UC3);
+    growAndMerge.rg_seg(image, mask, seeds);
 
+
+//    cv::namedWindow("Segmentation", cv::WINDOW_NORMAL);
+    cv::imshow("Segmentation", mask);
+    cv::waitKey(0);
 
     return 0;
 }
